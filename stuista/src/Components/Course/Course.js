d14b8d8fc6bd4c253f2da2d354686d50c7ee8b47
coderner <shreyashukla680@gmail.com>
2021-11-10 22:32:07 +0530

    added course intro layout

---
+++ stuista/src/Components/Course/Course.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Course.css";
+
+
+const Course = () =>{
+  return(
+     <div>   
+           <div className="Coursebg">
+                <div  className="CourseIntro">
+                                <div className="CourseIntroLines">  
+                                   <h5>NANODEGREE PROGRAM</h5>
+                                   <h1>Become a Data Engineer</h1>
+                                   <p>Data Engineering is the foundation for the new world of Big Data. Enroll now to build <br/>
+                                    production-ready data infrastructure, an essential skill for advancing your data career.</p>
+                                   <button className="EnrollNow">ENROLL NOW</button>
+                                </div>
+                </div>
+                <div className="I">
+                         <div>
+
+                         </div>
+                </div>
+           </div>
+             
+     </div>
+  )
+}
+
+export default Course;
