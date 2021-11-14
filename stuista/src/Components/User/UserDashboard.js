0a247d9417ac5807b576b4ce50e096bfbe8d23e0
coderner <shreyashukla680@gmail.com>
2021-11-15 04:06:54 +0530

    Created User Dashboard

--- stuista/src/Components/User/UserDashboard.js
+++ stuista/src/Components/User/UserDashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./User.css";
+import "../Instructor/Instructor.css";
 import { Link} from "react-router-dom";
 import profile from "../Images/CourseImages/Avatar3.svg";
+import CoursegridImage from "../Images/coursegrid.png";
 
 const UserDashboard= () => {
 return(
@@ -11,15 +13,45 @@ return(
                      <div className="userContent">
                         <div className="ProfileAvatar"><figure><img src={profile}/></figure></div>
                         <div className="userInfo">
-                          <h1>Data Engineering</h1>
-                          <p>email</p>
+                            <h1>Hi Shreya Shukla</h1>
+                            <h2>shreya@gmail.com</h2>
                         </div>
-          
-                   </div>
-           </div>
+                     </div>
+                  </div>
+
+                  <div className="InstructorsCourses">
+                      <div className="CoursesHeading">
+                         <h1>ALL COURSE</h1>
+                          <div className="Toggle">
+                             <h3>My learnings</h3>
+                             <h3>My Wishlist</h3>
+                          </div>
+                      </div>
+                     <div className="InstructorCourses">
+                           <div className="coursecard">
+                             <figure><img src={CoursegridImage} alt="Course pic" className="carouselimage" /></figure>
+                             <h5>Intro Data Analysis</h5>
+                             <p >Progress</p>
+                             <p>1 Month</p>
+                           </div>
+                           <div className="coursecard">
+                             <figure><img src={CoursegridImage} alt="Course pic" className="carouselimage" /></figure>
+                             <h5>Intro Data Analysis</h5>
+                             <p >Progress</p>
+                             <p>1 Month</p>
+                           </div>
+                           <div className="coursecard">
+                             <figure><img src={CoursegridImage} alt="Course pic" className="carouselimage" /></figure>
+                             <h5>Intro Data Analysis</h5>
+                             <p >Progress</p>
+                             <p>1 Month</p>
+                           </div>
+                     </div>
+              </div>
+              
 
 
-       </div>
+        </div>
               
     )
 }
